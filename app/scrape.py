bf31786407300ae4a15497d89e73b4e8828fe113
Guilherme <guilhermecfernandes@hotmail.com>
2022-05-25 18:55:51 -0300

    filtering news with more than 100 votes

--- app/scrape.py
+++ app/scrape.py
@@ -1,4 +1,6 @@
+from os import link
 import requests
+import pprint
 from bs4 import BeautifulSoup
 from environment import URL
 
@@ -17,5 +19,29 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # Grab the hyper link of every story with a score greater than 100
 # Grab all the elements with the css class '.storylink' -> Story Titles
 # Grab all the elements with the css classe '.score' -> Story scores
-links = soup.select('.storylink')
-votes = soup.select('.score')
+links = soup.select('.titlelink')
+# votes = soup.select('.score')
+subtext = soup.select('.subtext')
+
+
+def create_custom_website(links, subtext):
+    website = []
+    for index, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                website.append(
+                    {
+                        'title': title,
+                        'link': href,
+                        'votes': points
+                    }
+                )
+
+    return website
+
+
+pprint.pprint(create_custom_website(links, subtext))
