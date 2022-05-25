36e28872a4e567d4031ae3bd31d201b341e85678
Guilherme <guilhermecfernandes@hotmail.com>
2022-05-25 19:35:33 -0300

    adding requests for paginations

--- app/environment.py
+++ app/environment.py
@@ -1,1 +1,5 @@
-URL = 'https://news.ycombinator.com/news'
+URL = [
+    'https://news.ycombinator.com/news',
+    'https://news.ycombinator.com/news?p=2',
+    'https://news.ycombinator.com/news?p=3'
+]
--- app/scrape.py
+++ app/scrape.py
@@ -4,27 +4,55 @@ import pprint
 from bs4 import BeautifulSoup
 from environment import URL
 
-response = requests.get(url=URL)
-# print(response.text)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-# Selecting a CSS element (selector)
-# '.score' -> the css class called score
-# '#score_31497765' -> the css id called score_31497765
-# print(soup.find(id='score_31497765'))
-# print(soup.title)
-# print(soup.select('.score'))
+def get_website_object(url):
+    # TODO
+    # Request the html of a website
+    # Parser it into beautifulsoup4 object
 
-# TODO
-# Grab the hyper link of every story with a score greater than 100
-# Grab all the elements with the css class '.storylink' -> Story Titles
-# Grab all the elements with the css classe '.score' -> Story scores
-links = soup.select('.titlelink')
-# votes = soup.select('.score')
-subtext = soup.select('.subtext')
+    response = requests.get(url=url)
+    # print(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Selecting a CSS element (selector)
+    # '.score' -> the css class called score
+    # '#score_31497765' -> the css id called score_31497765
+    # print(soup.find(id='score_31497765'))
+    # print(soup.title)
+    # print(soup.select('.score'))
+    return soup
+
+
+def get_website_links(soup_object):
+    # TODO
+    # Grab all the elements with the css class '.titlelink' -> Story Titles
+    links = soup_object.select('.titlelink')
+    print(f'type of links {type(links)}')
+    return links
+
+
+def get_website_subtext(soup_object):
+    # TODO
+    # Grab all the elements with the css classe '.score' -> Story scores
+    # votes = soup.select('.score')
+    subtext = soup_object.select('.subtext')
+    print(f'type subtext {type(subtext)}')
+    return subtext
+
+
+def sort_stories_by_votes(list):
+    # TODO
+    # Sort list of objects ordered by votes in reverse
+    return sorted(
+        list,
+        key=lambda k: k['votes'],
+        reverse=True
+    )
 
 
 def create_custom_website(links, subtext):
+    # TODO
+    # Grab the hyper link of every story with a score greater than 100
     website = []
     for index, item in enumerate(links):
         title = item.getText()
@@ -41,7 +69,18 @@ def create_custom_website(links, subtext):
                     }
                 )
 
-    return website
+    return sort_stories_by_votes(website)
 
 
-pprint.pprint(create_custom_website(links, subtext))
+if __name__ == '__main__':
+    links = []
+    subtext = []
+    i = 1
+    for page in URL:
+        print(f'iteracao {i}')
+        soup = get_website_object(page)
+        links = links + get_website_links(soup)
+        print(f'type links for {links}')
+        subtext = subtext + get_website_subtext(soup)
+        i = i+1
+    pprint.pprint(create_custom_website(links, subtext))
